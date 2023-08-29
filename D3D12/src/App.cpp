8f6575628293a6a612543f4417774c55ad79a88a
Nastihan Beheshtypour <nastihanb@gmail.com>
2023-08-29 19:46:51 +0330

    fix COMMAND_LIST_MULTIPLE_SWAPCHAIN_BUFFER_REFERENCES

--- D3D12/src/App.cpp
+++ D3D12/src/App.cpp
@@ -1,4 +1,5 @@
 #include "App.h"
+#include <DirectXColors.h>
 
 App::App(HINSTANCE hInstance)
     : D3DApp(hInstance)
@@ -29,5 +30,36 @@ void App::Update(const GameTimer& gt)
 
 void App::Draw(const GameTimer& gt)
 {
+    ThrowIfFailed(pCmdListAlloc->Reset());
+    ThrowIfFailed(pCommandList->Reset(pCmdListAlloc.Get(), nullptr));
+
+    pCommandList->ResourceBarrier(1, &CD3DX12_RESOURCE_BARRIER::Transition(
+        CurrentBackBuffer(), D3D12_RESOURCE_STATE_PRESENT, D3D12_RESOURCE_STATE_RENDER_TARGET
+    ));
+
+    pCommandList->RSSetViewports(1, &screenViewport);
+    pCommandList->RSSetScissorRects(1, &scissorRect);
+
+    pCommandList->ClearRenderTargetView(
+        CurrentBackBufferView(), DirectX::Colors::Aquamarine, 0, nullptr);
+    pCommandList->ClearDepthStencilView(
+        DepthStencilView(), D3D12_CLEAR_FLAG_DEPTH | D3D12_CLEAR_FLAG_STENCIL, 1.0f, 0, 0, nullptr);
+
+    pCommandList->OMSetRenderTargets(
+        1, &CurrentBackBufferView(), true, &DepthStencilView());
+
+    pCommandList->ResourceBarrier(1, &CD3DX12_RESOURCE_BARRIER::Transition(
+        CurrentBackBuffer(), D3D12_RESOURCE_STATE_RENDER_TARGET, D3D12_RESOURCE_STATE_PRESENT
+    ));
+
+    ThrowIfFailed(pCommandList->Close());
+
+    ID3D12CommandList* cmdLists[] = { pCommandList.Get() };
+    pCommandQueue->ExecuteCommandLists(_countof(cmdLists), cmdLists);
+
+    pSwapChain->Present(0U, 0U);
+    currBackBuffer = (currBackBuffer + 1) % SwapChainBufferCount;
+
+    FlushCommandQueue();
 
 }
