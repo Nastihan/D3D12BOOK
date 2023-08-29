4895969d59c5d9d42becadeaea5e93c849906546
Nastihan Beheshtypour <nastihanb@gmail.com>
2023-08-29 15:31:42 +0330

    winmain change

--- D3D12/src/WinMain.cpp
+++ D3D12/src/WinMain.cpp
@@ -1,8 +1,20 @@
 #include <Windows.h>
-
+#include "Common/D3DApp.h"
 int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-	while (true);
-	return 0;
+#if defined(DEBUG) | defined(_DEBUG)
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
+
+	try
+	{
+		//D3DApp app(hInstance);
+	}
+	catch (DxException& e)
+	{
+		MessageBox(nullptr, e.ToString().c_str(), L"HR Failed", MB_OK);
+		return 0;
+	}
+
 }
 
